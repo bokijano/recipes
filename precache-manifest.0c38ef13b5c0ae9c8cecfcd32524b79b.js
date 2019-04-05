3dacdc3329670414ea9680ba0ffc11c3b9a56daa
Bojan Krstic <krstic.bojan80@gmail.com>
2019-04-05 15:00:03 +0200

    Updates

---
+++ precache-manifest.0c38ef13b5c0ae9c8cecfcd32524b79b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcbff70afda5869e8e5c",
+    "url": "/recipes-and-food-shop/static/js/runtime~main.cc3f3948.js"
+  },
+  {
+    "revision": "2082aae7085a3166050f",
+    "url": "/recipes-and-food-shop/static/js/main.a95a91d2.chunk.js"
+  },
+  {
+    "revision": "aa0e303fb8f7942e8ee1",
+    "url": "/recipes-and-food-shop/static/js/2.596a1f87.chunk.js"
+  },
+  {
+    "revision": "2082aae7085a3166050f",
+    "url": "/recipes-and-food-shop/static/css/main.f4cbb3e0.chunk.css"
+  },
+  {
+    "revision": "c5e7645b05bc4daa5d853efcfd45fb78",
+    "url": "/recipes-and-food-shop/index.html"
+  }
+];
