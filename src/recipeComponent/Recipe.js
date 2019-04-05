4f5e3a6c5b2ba914d3cd64f1c1ce7de607b3bc36
Bojan Krstic <krstic.bojan80@gmail.com>
2019-04-05 16:03:02 +0200

    second deploy

--- src/recipeComponent/Recipe.js
+++ src/recipeComponent/Recipe.js
@@ -2,8 +2,33 @@ import React, { Component } from "react";
 import { Image } from "react-bootstrap";
 
 export default class Recipe extends Component {
+  state = {
+    image: [
+      "./pictures/1.jpg",
+      "./pictures/2.jpg",
+      "./pictures/3.jpg",
+      "./pictures/4.jpg",
+      "./pictures/5.jpg",
+      "./pictures/6.jpg",
+      "./pictures/7.jpg",
+      "./pictures/8.jpg",
+      "./pictures/9.jpg",
+      "./pictures/10.jpg",
+      "./pictures/11.jpg",
+      "./pictures/12.jpg",
+      "./pictures/13.jpg",
+      "./pictures/14.jpg",
+      "./pictures/15.jpg",
+      "./pictures/16.jpg",
+      "./pictures/17.jpg",
+      "./pictures/18.jpg",
+      "./pictures/19.jpg",
+      "./pictures/20.jpg"
+    ]
+  };
   render() {
     const { title, image_src, id } = this.props.recipes;
+    const { image } = this.state;
     return (
       <React.Fragment>
         <div
@@ -14,7 +39,7 @@ export default class Recipe extends Component {
             <Image
               style={{ height: "14rem" }}
               className="img-card-top"
-              src={image_src}
+              src={image[id - 1]}
               rounded
               fluid
             />
--- src/recipeComponent/RecipeDetails.js
+++ src/recipeComponent/RecipeDetails.js
@@ -3,9 +3,34 @@ import { recipes } from "./../recipeList";
 import { Image } from "react-bootstrap";
 
 export default class RecipeDetails extends Component {
+  state = {
+    image: [
+      "./pictures/1.jpg",
+      "./pictures/2.jpg",
+      "./pictures/3.jpg",
+      "./pictures/4.jpg",
+      "./pictures/5.jpg",
+      "./pictures/6.jpg",
+      "./pictures/7.jpg",
+      "./pictures/8.jpg",
+      "./pictures/9.jpg",
+      "./pictures/10.jpg",
+      "./pictures/11.jpg",
+      "./pictures/12.jpg",
+      "./pictures/13.jpg",
+      "./pictures/14.jpg",
+      "./pictures/15.jpg",
+      "./pictures/16.jpg",
+      "./pictures/17.jpg",
+      "./pictures/18.jpg",
+      "./pictures/19.jpg",
+      "./pictures/20.jpg"
+    ]
+  };
   render() {
+    const { image } = this.state;
     const { handleIndex } = this.props;
-    const { image_src, title, ingredients } = this.props.recipes;
+    const { id, title, ingredients } = this.props.recipes;
     return (
       <React.Fragment>
         <div className="container">
@@ -18,7 +43,7 @@ export default class RecipeDetails extends Component {
               >
                 Back to recipe list
               </button>
-              <Image src={image_src} fluid rounded />
+              <Image src={image[id - 1]} fluid rounded />
             </div>
             <div className="col-10 mx-auto col-md-6 my-6">
               <h2 style={{ fontWeight: "bolder" }} className="fontTwo">
