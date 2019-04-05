009ccc9c33563ca7b556eacf0bdb958d9114df36
Bojan Krstic <krstic.bojan80@gmail.com>
2019-04-05 16:40:44 +0200

    new deploy

--- src/recipeComponent/Recipe.js
+++ src/recipeComponent/Recipe.js
@@ -1,33 +1,52 @@
 import React, { Component } from "react";
-import { Image } from "react-bootstrap";
+import Image_1 from "./pictures/1.jpg";
+import Image_2 from "./pictures/2.jpg";
+import Image_3 from "./pictures/3.jpg";
+import Image_4 from "./pictures/4.jpg";
+import Image_5 from "./pictures/5.jpg";
+import Image_6 from "./pictures/6.jpg";
+import Image_7 from "./pictures/7.jpg";
+import Image_8 from "./pictures/8.jpg";
+import Image_9 from "./pictures/9.jpg";
+import Image_10 from "./pictures/10.jpg";
+import Image_11 from "./pictures/11.jpg";
+import Image_12 from "./pictures/12.jpg";
+import Image_13 from "./pictures/13.jpg";
+import Image_14 from "./pictures/14.jpg";
+import Image_15 from "./pictures/15.jpg";
+import Image_16 from "./pictures/16.jpg";
+import Image_17 from "./pictures/17.jpg";
+import Image_18 from "./pictures/18.jpg";
+import Image_19 from "./pictures/19.jpg";
+import Image_20 from "./pictures/20.jpg";
 
 export default class Recipe extends Component {
   state = {
     image: [
-      "./pictures/1.jpg",
-      "./pictures/2.jpg",
-      "./pictures/3.jpg",
-      "./pictures/4.jpg",
-      "./pictures/5.jpg",
-      "./pictures/6.jpg",
-      "./pictures/7.jpg",
-      "./pictures/8.jpg",
-      "./pictures/9.jpg",
-      "./pictures/10.jpg",
-      "./pictures/11.jpg",
-      "./pictures/12.jpg",
-      "./pictures/13.jpg",
-      "./pictures/14.jpg",
-      "./pictures/15.jpg",
-      "./pictures/16.jpg",
-      "./pictures/17.jpg",
-      "./pictures/18.jpg",
-      "./pictures/19.jpg",
-      "./pictures/20.jpg"
+      Image_1,
+      Image_2,
+      Image_3,
+      Image_4,
+      Image_5,
+      Image_6,
+      Image_7,
+      Image_8,
+      Image_9,
+      Image_10,
+      Image_11,
+      Image_12,
+      Image_13,
+      Image_14,
+      Image_15,
+      Image_16,
+      Image_17,
+      Image_18,
+      Image_19,
+      Image_20
     ]
   };
   render() {
-    const { title, image_src, id } = this.props.recipes;
+    const { title, id } = this.props.recipes;
     const { image } = this.state;
     return (
       <React.Fragment>
@@ -36,12 +55,11 @@ export default class Recipe extends Component {
           className="col-10 mx-auto col-md-6 col-lg-3 my-3"
         >
           <div className="card">
-            <Image
+            <img
               style={{ height: "14rem" }}
-              className="img-card-top"
+              className="img-card-top img-responsive img-rounded"
               src={image[id - 1]}
-              rounded
-              fluid
+              alt="recipe"
             />
             <h2 style={{ textAlign: "center" }} className="fontTwo">
               {title}
--- src/recipeComponent/RecipeDetails.js
+++ src/recipeComponent/RecipeDetails.js
@@ -1,30 +1,49 @@
 import React, { Component } from "react";
-import { recipes } from "./../recipeList";
 import { Image } from "react-bootstrap";
+import Image_1 from "./pictures/1.jpg";
+import Image_2 from "./pictures/2.jpg";
+import Image_3 from "./pictures/3.jpg";
+import Image_4 from "./pictures/4.jpg";
+import Image_5 from "./pictures/5.jpg";
+import Image_6 from "./pictures/6.jpg";
+import Image_7 from "./pictures/7.jpg";
+import Image_8 from "./pictures/8.jpg";
+import Image_9 from "./pictures/9.jpg";
+import Image_10 from "./pictures/10.jpg";
+import Image_11 from "./pictures/11.jpg";
+import Image_12 from "./pictures/12.jpg";
+import Image_13 from "./pictures/13.jpg";
+import Image_14 from "./pictures/14.jpg";
+import Image_15 from "./pictures/15.jpg";
+import Image_16 from "./pictures/16.jpg";
+import Image_17 from "./pictures/17.jpg";
+import Image_18 from "./pictures/18.jpg";
+import Image_19 from "./pictures/19.jpg";
+import Image_20 from "./pictures/20.jpg";
 
-export default class RecipeDetails extends Component {
+export default class Recipe extends Component {
   state = {
     image: [
-      "./pictures/1.jpg",
-      "./pictures/2.jpg",
-      "./pictures/3.jpg",
-      "./pictures/4.jpg",
-      "./pictures/5.jpg",
-      "./pictures/6.jpg",
-      "./pictures/7.jpg",
-      "./pictures/8.jpg",
-      "./pictures/9.jpg",
-      "./pictures/10.jpg",
-      "./pictures/11.jpg",
-      "./pictures/12.jpg",
-      "./pictures/13.jpg",
-      "./pictures/14.jpg",
-      "./pictures/15.jpg",
-      "./pictures/16.jpg",
-      "./pictures/17.jpg",
-      "./pictures/18.jpg",
-      "./pictures/19.jpg",
-      "./pictures/20.jpg"
+      Image_1,
+      Image_2,
+      Image_3,
+      Image_4,
+      Image_5,
+      Image_6,
+      Image_7,
+      Image_8,
+      Image_9,
+      Image_10,
+      Image_11,
+      Image_12,
+      Image_13,
+      Image_14,
+      Image_15,
+      Image_16,
+      Image_17,
+      Image_18,
+      Image_19,
+      Image_20
     ]
   };
   render() {
@@ -43,7 +62,7 @@ export default class RecipeDetails extends Component {
               >
                 Back to recipe list
               </button>
-              <Image src={image[id - 1]} fluid rounded />
+              <Image src={image[id - 1]} rounded fluid />
             </div>
             <div className="col-10 mx-auto col-md-6 my-6">
               <h2 style={{ fontWeight: "bolder" }} className="fontTwo">
@@ -53,7 +72,7 @@ export default class RecipeDetails extends Component {
                 <h3 className="fontFive">Ingredients</h3>
                 {ingredients.map((item, index) => {
                   return (
-                    <li key={item} className="list-group-item fontFour">
+                    <li key={index} className="list-group-item fontFour">
                       {item}
                     </li>
                   );
