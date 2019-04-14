07a4a5bccc40d02efc47042e2edc8f49bb3f5c6e
Bojan Krstic <krstic.bojan80@gmail.com>
2019-04-14 21:38:52 +0200

    first commit

--- src/CustomNavbar.js
+++ src/CustomNavbar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class CustomNavbar extends Component {
   render() {
     return (
-      <div className="custom-look">Recipes fot your favourite meals !</div>
+      <div className="custom-look">Recipes for your favourite meals !</div>
     );
   }
 }
